f1f9a9e285663b003d99bd1fe1398d716ea67b37
JaxForReal <jackpribis@gmail.com>
2016-12-01 21:51:26 +1300

    add skip option on unrender command

--- src/main/java/com/jaxforreal/botto/BottoCommands.java
+++ src/main/java/com/jaxforreal/botto/BottoCommands.java
@@ -15,21 +15,36 @@ public class BottoCommands {
         commands.put("unrender", new Command() {
             @Override
             public String getHelp() {
-                return "prints the source of the last LaTeX message";
+                return "Usage: `unrender`, prints the source of the last LaTeX message\n" +
+                        "`unrender <num>` skips num posts, and displays that latex source\n" +
+                        "eg. `unrender 1` will return the second to last latex post's source";
             }
 
             @Override
             public void execute(String text, String nick, String trip, Botto bot) {
+                int numToSkip = 0;
+                try {
+                    numToSkip = text.equals("") ? 0 : Integer.parseInt(text);
+                } catch (NumberFormatException e) {
+                    numToSkip = 0;
+                }
+                if (numToSkip < 0) numToSkip = 0;
+
                 for (HistoryEntry entry : bot.history) {
-                    if (entry.text.startsWith("$")) {
+                    if (entry.text.contains("$")) {
+                        //continue to fetch next if a skip is needed
+                        if(numToSkip > 0) {
+                            numToSkip --;
+                            continue;
+                        }
                         //bot.sendChat("found");
                         String escapedText = entry.text.replaceAll("\\$", "<dollar>");
-                        bot.sendChat("Message by " + entry.nick + " at " + Util.dateString(entry.time) + " " + escapedText);
+                        bot.sendChat("Message by " + entry.nick + " at " + Util.dateString(entry.time) + "\n" + escapedText);
                         //System.out.println("Message by " + entry.nick + " at " + Util.dateString(entry.time) + "\n" + entry.text);
                         return;
                     }
                 }
-                bot.sendChat("could not find latex message");
+                bot.sendChat("could not find any latex messages");
             }
         });
 
@@ -50,7 +65,7 @@ public class BottoCommands {
             }
         });
 
-        commands.put("help", new Command() {
+        Command help = new Command() {
             @Override
             public String getHelp() {
                 return "returns generic help into or help test";
@@ -58,8 +73,10 @@ public class BottoCommands {
 
             @Override
             public void execute(String text, String nick, String trip, Botto bot) {
-                if(text.equals("")) {
-                    String message = "Usage: help <command>\nBotto, by @jax#xh7Atl\nPreceed all commands with '" + Botto.trigger + "'.\n";
+                if (text.equals("")) {
+                    String message = "Usage: help <command>\n\n" +
+                            "Botto, by @jax#xh7Atl\n" +
+                            "Precede all commands with '" + Botto.trigger + "'.\n";
                     for (Map.Entry<String, Command> command : bot.commands.entrySet()) {
                         //only show user level commands
                         if (PrivilegeLevel.USER.outranksOrEqual(command.getValue().getPrivilegeLevel())) {
@@ -73,7 +90,9 @@ public class BottoCommands {
                     bot.sendChat(commands.get(text).getHelp());
                 }
             }
-        });
+        };
+        commands.put("help", help);
+        commands.put("h", help);
 
         return commands;
     }
